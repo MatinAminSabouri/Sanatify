aa8008ab2bcd64ff1d773b871b24c8f78c7b9a42
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2024-06-23 18:02:18 +0330

    implement sql helper for embedding sql queries

--- SANATIFY/Program.cs
+++ SANATIFY/Program.cs
@@ -1,15 +1,26 @@
+using SANATIFY.Data;
+using SANATIFY.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString != null) builder.Services.AddSingleton(new AppDbContext(connectionString));
+
+builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,8 +31,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
